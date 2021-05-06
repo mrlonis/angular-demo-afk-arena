ff90cb2f8193850d39be8aee98b7e08ddea54352
Matthew Lonis <mattlonis@gmail.com>
2021-05-06 16:15:25 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'afk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-demo-afk-arena';
+}
